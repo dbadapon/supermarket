ec95980c09fc47599029696352866c790aaf27a5
Alvin Magee <alvinmagee@MacBook-Pro-30.dhcp.thefacebook.com>
2017-07-10 11:49:05 -0700

    Added User and Post Classes

---
+++ Supermarket/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  Supermarket
+//
+//  Created by Alvin Magee on 7/10/17.
+//  Copyright © 2017 Team Triceratops. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    
+}
