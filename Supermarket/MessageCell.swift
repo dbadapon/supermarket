7c1c0578b0f57e3052e1fe5ad096df8f39bdec44
Alvin Magee <alvinmagee@MacBook-Pro-30.dhcp.thefacebook.com>
2017-07-11 13:59:41 -0700

    Implemented Segmented Control on the notification screen

---
+++ Supermarket/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  Supermarket
+//
+//  Created by Alvin Magee on 7/11/17.
+//  Copyright © 2017 Team Triceratops. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Supermarket/NotificationsViewController.swift
@@ -0,0 +1,74 @@
+//
+//  NotificationsViewController.swift
+//  Supermarket
+//
+//  Created by Alvin Magee on 7/11/17.
+//  Copyright © 2017 Team Triceratops. All rights reserved.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 100
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            return 20
+        } else {
+            return 20
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if segmentedControl.selectedSegmentIndex == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "InterestedCell") as! InterestedCell
+            return cell
+            
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell") as! MessageCell
+            return cell
+            
+        }
+        
+    }
+
+    @IBAction func indexChanged(_ sender: Any) {
+        tableView.reloadData()
+        
+        // print ("inded changed \(segmentedControl.selectedSegmentIndex)")
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
