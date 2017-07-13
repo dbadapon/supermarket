b87c02ee4cf7f9b7d2c760b720df145f7c2e1921
Alvin Magee <alvinmagee@MacBook-Pro-30.dhcp.thefacebook.com>
2017-07-13 13:28:10 -0700

    Created MLManager class and added placeholder function to return a String when given an image

---
+++ Supermarket/MLManager.swift
@@ -0,0 +1,16 @@
+//
+//  MLManager.swift
+//  Supermarket
+//
+//  Created by Alvin Magee on 7/13/17.
+//  Copyright © 2017 Team Triceratops. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MLManager {
+    static func getName(withImage image: UIImage) -> String {
+        return "Apple"
+    }
+}
